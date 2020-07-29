d9cab9b5464b418931bfa970594cb6107d1faadc
Cody C <alix.henry@ymail.com>
2020-07-28 21:59:29 -0500

    trying to get the like count to increase when onClick is called

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -2,17 +2,14 @@
 // Use a piece of data coming in through props to display the correct number of likes.
 // You will also add an onClick handler that utilizes `likePost` to increase the count of likes.
 // (As a stretch goal, you might want to prevent your user from "liking" the same post more than once.)
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
   const { likePost, numberOfLikes  } = props;
-
-  const likeCounter = () => {
-    return (numberOfLikes + 1);
-  }
+  const { likeButton, setLikeButton } = useState(numberOfLikes);
 
   return (
     <div>
@@ -21,7 +18,7 @@ const LikeSection = props => {
         key='likes-icons-container'
       >
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} onClick = {() => likePost(likeCounter)} />
+          <FontAwesomeIcon icon={faHeart} onClick = {() => setLikeButton(likeButton + 1)} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
